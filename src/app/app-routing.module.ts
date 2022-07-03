45e3df572668df235477037b38a4476f1815f112
Bulcki <vgzalexis@gmail.com>
2022-07-03 18:16:09 +0200

    chore: first section html care-u

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from 'src/app/home/home.component';
+import { CareUComponent } from './care-u/care-u.component';
+import { DressMeSlowComponent } from './dress-me-slow/dress-me-slow.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent}
+  { path: '', component: HomeComponent},
+  { path: 'care-u', component: CareUComponent},
+  { path: 'dress-me-slow', component: DressMeSlowComponent}
+
 ];
 
 @NgModule({
